c48f2697822ea937c84e6a141f4bd0be583ada93
Alba Maitia Vadillo Valderrama <alba.vadillo3@gmail.com>
2025-03-15 20:48:57 +0100

    pdf

--- 04.Documents_loaders/pdf_loader.py
+++ 04.Documents_loaders/pdf_loader.py
@@ -1,9 +1,33 @@
 from langchain_community.document_loaders import PyMuPDFLoader
+import os
+
 
 loader = PyMuPDFLoader("rag-dataset/health supplements/1. dietary supplements - for whom.pdf")
 
 docs = loader.load()
-len(docs)
+#print("docs->", docs[0])
+#print(docs[0].__dict__)
+#print("doc metadata -> ",docs[0].metadata)
+#print(docs[0].page_content)
 
-docs[0].metadata
-print(docs[0].page_content)
+### Read the list of PDFs in the dir
+pdfs = []
+for root, dirs, files in os.walk("rag-dataset"):
+    # print(root, dirs, files)
+    for file in files:
+        if file.endswith(".pdf"):
+            pdfs.append(os.path.join(root, file))
+'''
+pdfs= [
+    "rag-dataset/document1.pdf",
+    "rag-dataset/supplements/report.pdf"
+]git 
+'''
+docs = []
+for pdf in pdfs:
+    loader = PyMuPDFLoader(pdf)
+    temp = loader.load()
+    docs.extend(temp)
+
+    #print(temp)
+    #break
