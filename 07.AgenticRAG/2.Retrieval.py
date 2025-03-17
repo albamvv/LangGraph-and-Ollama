caa0c53319c6b699f132eb5282bcd9ec937a50af
Alba Maitia Vadillo Valderrama <alba.vadillo3@gmail.com>
2025-03-17 15:30:52 +0100

    retrieval

--- 07.AgenticRAG/2.Retrieval.py
+++ 07.AgenticRAG/2.Retrieval.py
@@ -1,30 +1,25 @@
 from imports import*
-
-embeddings = OllamaEmbeddings(model='nomic-embed-text', base_url='http://localhost:11434')
-
-db_name = r"D:\NLP\LLM\Langchain and Ollama\09. Vector Stores and Retrievals\health_supplements"
-vector_store = FAISS.load_local(db_name, embeddings, allow_dangerous_deserialization=True)
+from config import embeddings,db_name, vector_store
 
 
 ### Retreival
 question = "how to gain muscle mass?"
+# This line performs a search in the vector_store to find documents that are most similar to the provided question.
 docs = vector_store.search(query=question, k=5, search_type="similarity")
-print(docs)
-retriever = vector_store.as_retriever(search_type = 'similarity', 
-                                      search_kwargs = {'k': 3})
-
-retriever.invoke(question)
-
+# This line creates a retriever object from the vector_store. The retriever is responsible for fetching relevant documents based on a query.
+retriever = vector_store.as_retriever(search_type = 'similarity', search_kwargs = {'k': 3})
+response=retriever.invoke(question)
+#--------------------------------
 question = "how to lose weight?"
-retriever.invoke(question)
+response=retriever.invoke(question)
+#--------------------------------
+retriever = vector_store.as_retriever(search_type = 'similarity_score_threshold', search_kwargs = {'k': 3, 'score_threshold': 0.1})
+response= retriever.invoke(question)
+#--------------------------------
+retriever = vector_store.as_retriever(search_type = 'mmr', search_kwargs = {'k': 3, 'fetch_k': 20, 'lambda_mult': 1})
+response = retriever.invoke(question)
 
-retriever = vector_store.as_retriever(search_type = 'similarity_score_threshold', 
-                                      search_kwargs = {'k': 3, 'score_threshold': 0.1})
 
-retriever.invoke(question)
-
-retriever = vector_store.as_retriever(search_type = 'mmr', 
-                                      search_kwargs = {'k': 3, 'fetch_k': 20, 'lambda_mult': 1})
-
-docs = retriever.invoke(question)
-docs
+for doc in response:
+    print(f"📄 Source: {doc.metadata['source']} (Page {doc.metadata['page']})")
+    print(f"📌 Content: {doc.page_content}\n")
